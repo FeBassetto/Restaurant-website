d61588ec282ffd7781fd435c58e82efbb11a22dc
Felipe Bassetto <felipebtu9@gmail.com>
2022-04-13 11:58:44 -0300

    Imgs Home added to folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Routing from "./routes";
+import { ThemeProvider } from "@material-ui/core";
+
 
 function App() {
+
+  const themes = {
+    colors:{
+      //white
+      white:"#ffffff",
+      //black
+      black:"#000000",
+      //grey
+      greyMenu:"#C8BFBF",
+      lightGreyBackground:"#C4C4C4",
+      darkGreyTxt:"#4D4D4D",
+      darkGreyBorder:"#414141",
+      //red
+      red:"#C90000",
+      //yellow
+      lightYellow:"#FAAB34",
+      darkYellow:"#D88200",
+      gold:"#D88200",
+      //green
+      green:"#017E0D"
+    }
+  }
+
   return (
     <div className="App">
-      
+      <ThemeProvider theme={themes}>
+        <Routing />
+      </ThemeProvider>
     </div>
   );
 }
