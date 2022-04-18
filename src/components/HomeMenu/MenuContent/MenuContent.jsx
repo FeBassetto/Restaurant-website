f01b0a06795d10cfa2a9f094c499db67e3df6aba
Felipe Bassetto <felipebtu9@gmail.com>
2022-04-18 10:16:01 -0300

    Finished Context in MenuContent

--- src/components/HomeMenu/MenuContent/MenuContent.jsx
+++ src/components/HomeMenu/MenuContent/MenuContent.jsx
@@ -1,11 +1,6 @@
 import React, { useContext } from "react";
 import './MenuContent.css';
 import BurgerBox from "../../BurgerBox/BurgerBox";
-import Burger_1 from '../../../assets/img/burgerProduct_1.png'
-import Burger_2 from '../../../assets/img/burgerProduct_2.png'
-import Burger_3 from '../../../assets/img/burgerProduct_3.png'
-import Burger_4 from '../../../assets/img/burgerProduct_4.png'
-import Burger_5 from '../../../assets/img/burgerProduct_5.png'
 import Burger_6 from '../../../assets/img/burgerProduct_6.png'
 import { SnackContext } from './../../../common/context/snacks';
 
